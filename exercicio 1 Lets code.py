0c3d8341249c34a015b29c298cd4b224fdbcbe27
Fabrício Formentini <91196904+fabriciohof@users.noreply.github.com>
2022-02-07 15:14:23 -0300

    Add files via upload

---
+++ exercicio 1 Lets code.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça um valor monetário e diminua-o em 15%.
+# Seu programa deve imprimir a mensagem “O novo valor é [valor]”.
+
+valor = float(input('Digite aqui seu valor monetário: '))
+valor = (valor) -(valor*0.15)
+print('O seu novo valor é: ', valor)
